4e355490587727dc62935fa9782dfe71d014a0c8
Hondlus <48700890+Hondlus@users.noreply.github.com>
2025-11-21 08:43:58 +0800

    Update install.py

--- install.py
+++ install.py
@@ -1,4 +1,4 @@
 import os
 
-os.system("pip install playwright pandas openpyxl PyQt6 -i https://mirrors.aliyun.com/pypi/simple")
+os.system("powershell -Command \"pip install playwright pandas openpyxl PyQt6 'markitdown[pdf, docx, pptx, az-doc-intel]' magika -i https://mirrors.aliyun.com/pypi/simple\"")
 os.system("playwright install chromium")
