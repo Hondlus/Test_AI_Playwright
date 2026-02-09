2686d1b903edd174075bf6637712ea9cb93c7089
Hondlus <48700890+Hondlus@users.noreply.github.com>
2026-02-09 15:17:03 +0800

    Update install.py

--- install.py
+++ install.py
@@ -4,4 +4,5 @@ os.system("powershell -Command \"pip install playwright==1.54.0 pandas==2.3.1 op
 os.system("playwright install chromium")
 # pyinstaller==6.16.0 pyinstaller-hooks-contrib==2025.8
 # 打包命令:pyinstaller -w -F .\neepshop_UI.py .\neepshop_main.py .\logger.py --add-data D:\ProgramData\Anaconda3\envs\py31293\Lib\site-packages\playwright:playwright/ --add-data D:\ProgramData\Anaconda3\envs\py31293\Lib\site-packages\markitdown:markitdown/ --add-data D:\ProgramData\Anaconda3\envs\py31293\Lib\site-packages\magika:magika
+# 打包命令2:pyinstaller -w -F .\neepshop_UI.py .\neepshop_main.py .\logger.py --add-data C:\Users\dxw-user\AppData\Local\ms-playwright:playwright --add-data C:\Users\DXW\AppData\Local\Programs\Python\Python312\Lib\site-packages\markitdown:markitdown/ --add-data C:\Users\DXW\AppData\Local\Programs\Python\Python312\Lib\site-packages\magika:magika
 
